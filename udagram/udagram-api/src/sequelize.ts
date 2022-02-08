e59dbe1b54b7070cf848646fad32c7cb85ab16c7
Trung Vo <trunglive@gmail.com>
2022-02-08 23:22:40 +0700

    chore: update start script #2

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -6,6 +6,7 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
+  port: 5432,
 
   dialect: "postgres",
   storage: ":memory:",
